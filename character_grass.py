687ab735c3f7a9e68d07aa9f8a4a31756358aef5
Tamach4n <jinhyung119@naver.com>
2024-09-24 12:15:45 +0900

    원 이동 함수 구현
    대 성 공

--- character_grass.py
+++ character_grass.py
@@ -9,20 +9,28 @@ x = 0
 y = 90
 
 def run_rectangle():
-    clear_canvas_now()
-    boy.draw_now(400, 90)
-    grass.draw_now(400, 30)
-    delay(0.1)
+    #clear_canvas_now()
+    #boy.draw_now(x + 400, y + 255)
+    #grass.draw_now(400, 30)
+    #delay(0.1)
+    pass
 
 def run_circle():
-    clear_canvas_now()
-    boy.draw_now(400, 90)
-    grass.draw_now(400, 30)
-    delay(0.1)
+    r, cx, cy = 250, 800 // 2, 600 // 2 + 40    #   화면 크기의 반
+    
+    for degree in range(0, 360, 3):
+        theta = math.radians(degree - 90)    #   theta -> rad
+        x = r * math.cos(theta) + cx
+        y = r * math.sin(theta) + cy
 
-while True: #   뼈대 잡는 것이 제일 중요
+        clear_canvas_now()
+        boy.draw_now(x, y)
+        grass.draw_now(400, 30)
+        
+        delay(0.01)
+
+while True:     #   뼈대 잡는 것이 제일 중요
     run_rectangle()
     run_circle()
-    break
 
 close_canvas()
