66fd382c4625bf52debc17c157b559948143ab28
Tamach4n <jinhyung119@naver.com>
2024-09-24 11:50:10 +0900

    뼈대 완성
    Print 루프로 기능 확인

---
+++ character_grass.py
@@ -0,0 +1,23 @@
+from pico2d import *
+import math
+
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+x = 0
+y = 90
+
+def run_rectangle():
+    print("Circle")
+    pass
+
+def run_circle():
+    print("Rectnagle")
+    pass    #   빈 함수 > 아무 일도 하지 않음  
+
+while True:
+    run_rectangle()
+    run_circle()    
+
+close_canvas()
