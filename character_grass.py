4a83aafa5da0c14a340d0b3bb43f4fd45b6f1860
Tamach4n <jinhyung119@naver.com>
2024-09-24 18:25:12 +0900

    삼각형 이동 구현하기
    대 성 공

--- character_grass.py
+++ character_grass.py
@@ -34,13 +34,9 @@ def tri_BC(s, e):
 
 def tri_AC():
     x, y = 700, 90
-    #theta = math.radians(60.64)
-    # mx = 2 * math.cos(theta)
-    # my = 2 * math.sin(theta)
-    
     dx, dy = -300, 400
 
-    length = math.sqrt(300 ** 2 + 400 ** 2)
+    length = math.sqrt(dx ** 2 + dy ** 2)
 
     mx = dx / length
     my = dy / length
@@ -50,10 +46,18 @@ def tri_AC():
         y += my * 2
         printImages(x, y)
 
-    print(x, y)
+def tri_AB():
+    x, y = 400, 490
+    dx, dy = -300, -400
 
-#def tri_AB():
-    #while 
+    length = math.sqrt(dx ** 2 + dy ** 2)
+
+    mx, my = dx / length, dy / length
+
+    while x > 101:
+        x += 2 * mx
+        y += 2 * my
+        printImages(x, y)
 
 def run_rectangle():
     run_rBottom(400, 780)
@@ -65,7 +69,7 @@ def run_rectangle():
 def run_circle():
     r, cx, cy = 250, 800 // 2, 600 // 2 + 40    #   화면 크기의 반
     
-    for degree in range(0, 360, 3):
+    for degree in range(0, 360, 1):
         theta = math.radians(degree - 90)    #   degree -> rad, -90 ~ 270
         x = r * math.cos(theta) + cx
         y = r * math.sin(theta) + cy
@@ -73,15 +77,15 @@ def run_circle():
         printImages(x, y)
 
 def run_triangle():
-   # tri_BC(400, 700)
+    tri_BC(400, 700)
     tri_AC()
-    #tri_AB()
-    #tri_BC(100, 400)
+    tri_AB()
+    tri_BC(100, 400)
     delay(5)
 
 while True:     #   뼈대 잡는 것이 제일 중요
-    #run_rectangle()
-    #run_circle()
+    run_rectangle()
+    run_circle()
     run_triangle()
     break
 
